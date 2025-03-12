335e391f672e764f79ee149a7068694ce83aa64b
Dhruv Patel <dhruv.n.patel@simformsolutions.com>
2025-03-12 15:30:09 +0530

    Update profile image URL and enhance tweet list template with user profile display

--- tweet/context_processors.py
+++ tweet/context_processors.py
@@ -8,6 +8,6 @@ def global_user_context(request):
         
         return {
             'auth_user': request.user,  # Authenticated user object
-            'profile_img': profile_img.url if profile_img else ""  # Profile image URL
+            'profile_img': profile_img.url if profile_img else "https://th.bing.com/th/id/OIP.0IjSh8YAMecKyLNyMRW90gHaHa?rs=1&pid=ImgDetMain"  # Profile image URL
         }
     return {}  # Return an empty dictionary if no user is logged in
